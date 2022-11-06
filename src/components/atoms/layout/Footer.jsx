1e59ca9fb8ba3e89d4bf421594d12d9219c45871
Kenta Kato <01keny01@gmail.com>
2022-11-06 23:25:07 +0900

    create Footer component

---
+++ src/components/atoms/layout/Footer.jsx
@@ -0,0 +1,7 @@
+export const Footer = () => {
+    return(
+        <div className='absolute bg-white w-full flex justify-center py-4 pl-4 border-t'>
+            <footer className='text-gray-600'>© Native Typing - 2022 ~</footer>
+        </div>
+    );
+}
