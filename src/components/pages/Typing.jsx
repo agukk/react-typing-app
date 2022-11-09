9ba9733141f4959471ada202e128e8915f3d8e30
Kenta Kato <01keny01@gmail.com>
2022-11-09 10:19:30 +0900

    add retry button and typing detail

--- src/components/pages/Typing.jsx
+++ src/components/pages/Typing.jsx
@@ -1,8 +1,11 @@
 import { useEffect, useState } from "react";
+// eslint-disable-next-line
+import axios from "axios";
 import { CorrectLetter } from "../atoms/layout/CorrectLetter";
 import { MissLetter } from "../atoms/layout/MissLetter";
 import { NomalLetter } from "../atoms/layout/NomalLetter";
 import { TypeLetter } from "../atoms/layout/TypeLetter";
+import { PrimaryButton } from "../atoms/button/PrimaryButton";
 
 export const Typing = () => {
     // words that user types
@@ -22,6 +25,9 @@ export const Typing = () => {
     const [typingString, setTypingString] = useState("");
     const [index, setIndex] = useState(0);
     const [isMissType, setIsMissType] = useState(false);
+    const [missCount, setMissCount] = useState(0);
+    const [finish, setFinish] = useState(false);
+    const [retry, setRetry] = useState("");
 
     useEffect(() => {
         let ts = '';
@@ -32,31 +38,57 @@ export const Typing = () => {
         let newTypingString = ts.slice(0,-1)
         setTypingString(newTypingString); 
         // eslint-disable-next-line 
-    },[]);
+    },[retry]);
 
     const checkTypingLetter = (event) => {
+        if (finish)
+        {
+            return;
+        }
         if (event.key === typingString[index])
         {
             setIsMissType(false);
             setIndex(index + 1);
+            if (index + 1 >= typingString.length)
+            {
+                setFinish(true);
+            }
         }
         else
         {
             setIsMissType(true);
+            setMissCount(missCount + 1);
         }
     }
+
+    const initilize = () => {
+        setIndex(0);
+        setIsMissType(false);
+        setMissCount(0);
+        setFinish(false);
+        setRetry(Math.random());
+    }
+
+    const accuracy = (index / (index + missCount) * 100).toFixed(1)
 
     return(
         <div className='h-screen w-screen flex flex-col justify-center items-center'>
             <div onKeyDown={(event) => checkTypingLetter(event)} tabIndex={0} className='text-5xl font-serif p-24 cursor-pointer'>
                 <CorrectLetter typingString={typingString} index={index}/>
-            
                 {isMissType ? (
                     <MissLetter typingString={typingString} index={index}/>
                 ) : (
                     <NomalLetter typingString={typingString} index={index}/>
                 )}
                 <TypeLetter typingString={typingString} index={index}/>
+            </div>
+            <div className='font-light text-2xl space-x-12'>
+                <span>CorrectType : { index }</span>
+                <span>MissType : { missCount }</span>
+                <span>Accuracy : { accuracy } %</span>
+            </div>
+            <div onClick={initilize} className='pt-12'>
+                <PrimaryButton>Retry</PrimaryButton>
             </div>
         </div>
     );
