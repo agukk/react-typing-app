9e021a79dcb497abf47b183d0260d1c8c173814b
Kenta Kato <01keny01@gmail.com>
2022-11-10 08:00:16 +0900

    create seconday button

--- src/components/atoms/button/SecondaryButton.jsx
+++ src/components/atoms/button/SecondaryButton.jsx
@@ -1,6 +1,7 @@
-export const SecondaryButton = () => {
+export const SecondaryButton = ({ children }) => {
     return(
-        <>
-        </>
+        <button className="bg-indigo-700 hover:bg-indigo-600 shadow-xl font-semibold text-xl text-white py-2 px-3 rounded">
+            { children }
+        </button>
     );
 }
