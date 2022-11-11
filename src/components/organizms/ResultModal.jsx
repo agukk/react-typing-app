609ba24b3cbcf6478e350edb5d41337dc14404d0
Kenta Kato <01keny01@gmail.com>
2022-11-11 22:51:29 +0900

    refactoring css code at resultModal.jsx

--- src/components/organizms/ResultModal.jsx
+++ src/components/organizms/ResultModal.jsx
@@ -10,18 +10,18 @@ export const ResultModal = ({correctCount, missCount, modalOpen, initilize, accu
                 <div className="relative z-10">
                     <div className="fixed inset-0 bg-gray-500 bg-opacity-75 transition-opacity"></div>
 
-                    <div className="fixed inset-0 z-10 overflow-y-auto">
+                    <div className="fixed inset-0 z-10 overflow-y-autow">
                         <div className="flex min-h-full items-end justify-center p-4 text-center sm:items-center sm:p-4">
-                            <div className="relative transform overflow-hidden rounded-lg bg-white shadow-xl transition-all sm:my-8 sm:w-5/6 sm:max-w-lg">
-                                <div className="p-5 bg-white border-b dark:border-gray-600">
+                            <div className="relative transform overflow-hidden rounded-lg bg-gray-50 shadow-xl transition-all sm:my-8 sm:w-full sm:max-w-md">
+                                <div className="p-5 border-b dark:border-gray-600">
                                     <ResultHeadline>Score : {((correctCount - missCount) * (accuracy / 100)).toFixed(0)}</ResultHeadline>
                                 </div>
-                                <div className="bg-white p-5 space-y-3">
+                                <div className="p-5 space-y-3">
                                     <ResultDetail>Correct Type : {correctCount}</ResultDetail>
                                     <ResultDetail>Miss Type : {missCount}</ResultDetail>
                                     <ResultDetail>Accuracy : {accuracy} %</ResultDetail>
                                 </div>
-                                <div className="bg-gray-50 flex justify-center pt-5 pb-5 space-x-16">
+                                <div className="flex justify-center pt-5 pb-10 px-10 space-x-16">
                                     <span onClick={initilize}>
                                         <SecondaryButton>Again</SecondaryButton>
                                     </span>
