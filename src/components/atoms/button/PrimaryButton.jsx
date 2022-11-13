d69a9e83a43fc22eef5943f2152b3026f3d0a409
Kenta Kato <01keny01@gmail.com>
2022-11-13 17:53:05 +0900

    change PrimaryButton style

--- src/components/atoms/button/PrimaryButton.jsx
+++ src/components/atoms/button/PrimaryButton.jsx
@@ -1,6 +1,6 @@
 export const PrimaryButton = ({ children }) => {
     return(
-        <button className="bg-indigo-700 hover:bg-indigo-600 shadow-xl font-semibold text-2xl text-white py-3 px-4 rounded">
+        <button className="py-4 px-6  bg-indigo-600 hover:bg-indigo-700 focus:ring-indigo-500 focus:ring-offset-indigo-200 text-white w-full transition ease-in duration-200 text-center text-base font-semibold shadow-md focus:outline-none focus:ring-2 focus:ring-offset-2  rounded-lg ">
             { children }
         </button>
     );
